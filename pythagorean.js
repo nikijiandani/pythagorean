5d5d66aa9a9e0402d0b813fad33b6bb4a1d84027
Niki Jiandani <niki.jiandani@gmail.com>
2019-05-30 16:28:28 +0000

    Add function to calculate the hypotenuse

---
+++ pythagorean.js
@@ -0,0 +1,11 @@
+var input = [
+    { x: 3, y: 4 },
+    { x: 12, y: 5 },
+    { x: 8, y: 15 }
+  ];
+  var result = input.map(/* Your code here */
+    (obj) => Math.sqrt((obj.x ** 2) + (obj.y ** 2)));
+  
+  console.log(result[0] === 5);
+  console.log(result[1] === 13);
+  console.log(result[2] === 17);
